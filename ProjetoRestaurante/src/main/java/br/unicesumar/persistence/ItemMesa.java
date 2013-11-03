95737d4f99a0942012d502d354020b47728b3a57
fsaguilieri <francielloa@ig.com.br>
2013-11-03 17:17:27 -0200

    Implementado ItemMesa

--- ProjetoRestaurante/src/main/java/br/unicesumar/persistence/ItemMesa.java
+++ ProjetoRestaurante/src/main/java/br/unicesumar/persistence/ItemMesa.java
@@ -1,5 +1,43 @@
 package br.unicesumar.persistence;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+
+@Entity
 public class ItemMesa {
+	
+	@Column
+	private int id;
+	
+	@Column
+	private int mesa;
+	
+	@Column
+	private double quantidade;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+	
+	public int getMesa() {
+		return mesa;
+	}
+
+	public void setMesa(int mesa) {
+		this.mesa = mesa;
+	}
+
+
+	public double getQuantidade() {
+		return quantidade;
+	}
+
+	public void setQuantidade(double quantidade) {
+		this.quantidade = quantidade;
+	}
 
 }
